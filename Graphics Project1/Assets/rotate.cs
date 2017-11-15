7d6ef1e13b370cb0dbc88b4c0c3a66a8333d3e13
JackDalton3110 <c00205943@itcarlow.ie>
2017-11-15 15:10:41 +0000

    Tetris shapes complete

---
+++ Graphics Project1/Assets/rotate.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class rotate : MonoBehaviour {
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+		if(Input.GetKeyDown(key::KeyCode.LeftArrow))
+	}
+}
