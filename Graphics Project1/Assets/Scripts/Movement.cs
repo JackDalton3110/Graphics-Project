109f409021694c1127929347a1c8f9f777980c2e
JackDalton3110 <c00205943@itcarlow.ie>
2017-11-27 12:37:27 +0000

    Coroutine for vertical movement

---
+++ Graphics Project1/Assets/Scripts/Movement.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Author: Jack Dalton
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class Movement : MonoBehaviour {
+
+    private Rigidbody rb;
+    public Boundary boundary;
+    public int speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine("move");
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Boundary"))
+        {
+        
+            StopCoroutine("move");
+
+        }
+
+    }
+
+    IEnumerator move()
+    {
+        for (;;)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - 4.0f, transform.localPosition.z);
+
+            yield return new WaitForSeconds(1);
+        }    
+    }
+}
