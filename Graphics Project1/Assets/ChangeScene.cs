be6301750361f3764e499c90628cf750747ab0ca
JackDalton3110 <c00205943@itcarlow.ie>
2017-12-02 19:34:17 +0000

    Play button working

---
+++ Graphics Project1/Assets/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Change : MonoBehaviour
+{
+
+	public void change(string NewGame)
+    {
+        SceneManager.LoadScene(NewGame);
+    }
+}
