4a11bf7ecc00737518d2e87b802c962af659b241
AaronCur <c00202505@itcarlow.ie>
2017-12-04 11:12:52 +0000

    improved player movement

--- Graphics Project1/Assets/Scripts/Tetramino.cs
+++ Graphics Project1/Assets/Scripts/Tetramino.cs
@@ -13,8 +13,19 @@ public class Tetramino : MonoBehaviour {
 
     public float blockScoreTime;
 
-	// Use this for initialization
-	void Start () {
+    private float continuousVerticalSpeed = 0.05f; //Speed the tetromino moves when the down button is held 
+    private float coninuousHorizontalSpeed = 0.1f; // Speed at which the teromino will move when the left or right 
+    private float buttonDownWait = 0.2f; //How long to wait befor the shape starts moving when it recognizes a button is held
+    private float buttonDownWaitTimer = 0; 
+
+    private float verticalTimer = 0.0f;
+    private float horizontalTimer = 0.0f;
+
+    private bool movedImmediateHorizontal = false;
+    private bool movedImmediateVertical = false;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 
@@ -37,9 +48,43 @@ public class Tetramino : MonoBehaviour {
         UpdateBlockScore();
 	}
     void CheckUserInput()
-    {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+    {  //To increment when player just hits the key once. to override timer code. e.g Sometimes the timer doesnt increment enough so shape wont move.
+    //Resets timers and immediate move bools  when player releases key
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.DownArrow))
         {
+            movedImmediateHorizontal = false;
+            movedImmediateVertical = false;
+            horizontalTimer = 0;
+            verticalTimer = 0;
+            buttonDownWaitTimer = 0;
+
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (movedImmediateHorizontal == true)
+            {
+
+
+                if (buttonDownWaitTimer < buttonDownWait)
+                {
+                    buttonDownWaitTimer += Time.deltaTime;
+                    return;
+                }
+                //Stepping movement
+                if (horizontalTimer < coninuousHorizontalSpeed)
+                {
+                    horizontalTimer += Time.deltaTime;
+                    //Get out of the method
+                    return;
+                }
+
+            }
+            if (movedImmediateHorizontal == false)
+            {
+                movedImmediateHorizontal = true;
+            }
+            horizontalTimer = 0;
             transform.position += new Vector3(1, 0, 0);
 
             if (CheckIsValidPosition())
@@ -52,8 +97,28 @@ public class Tetramino : MonoBehaviour {
             }
             
         }
-        else  if (Input.GetKeyDown(KeyCode.LeftArrow))
+        else  if (Input.GetKey(KeyCode.LeftArrow))
         {
+            if (movedImmediateHorizontal == true)
+            {
+                if (buttonDownWaitTimer < buttonDownWait)
+                {
+                    buttonDownWaitTimer += Time.deltaTime;
+                    return;
+                }
+                //Stepping movement
+                if (horizontalTimer < coninuousHorizontalSpeed)
+                {
+                    horizontalTimer += Time.deltaTime;
+                    //Get out of the method
+                    return;
+                }
+            }
+            if (movedImmediateHorizontal == false)
+            {
+                movedImmediateHorizontal = true;
+            }
+            horizontalTimer = 0;
             transform.position += new Vector3(-1, 0, 0);
 
             if (CheckIsValidPosition())
@@ -113,8 +178,28 @@ public class Tetramino : MonoBehaviour {
             }
 
         }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
+        else if (Input.GetKey(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
         {
+            if (movedImmediateVertical == true)
+            {
+
+
+                if (buttonDownWaitTimer < buttonDownWait)
+                {
+                    buttonDownWaitTimer += Time.deltaTime;
+                    return;
+                }
+                if (verticalTimer < continuousVerticalSpeed)
+                {
+                    verticalTimer += Time.deltaTime;
+                    return;
+                }
+            }
+            if (movedImmediateVertical == false)
+            {
+                movedImmediateVertical = true;
+            }
+            verticalTimer = 0;
             transform.position += new Vector3(0, -1, 0);
             if (CheckIsValidPosition())
             {
